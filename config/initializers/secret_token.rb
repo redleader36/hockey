12d9b46593c0ae412efa05e93a3b5184a4b53323
deckersports <webmaster@deckersports.biz>
2012-09-12 15:41:47 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hockey::Application.config.secret_token = '5ee2445576aa7e2ee68d193b036c2781f86e1f865f1c6b688304219e01fe4585285f7948cc0a6c82275f5c192c76930f9a76962b5f59ceb57a4490c4b349f51a'
